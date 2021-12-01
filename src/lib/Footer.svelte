8747d9234157caed77b305cd602efbf60af18286
suytds <95157873+suytds@users.noreply.github.com>
2021-12-01 07:30:24 -0700

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -128,11 +128,9 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
+	<span class="copyright">&copy; 2021 - {year} <a href="https://imgur.com/gallery/yoQJv">Show Us Your TDs</a></span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
-	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+	<span class="creator">Built by <a href="https://www.lockhaven.edu/~dsimanek/flat/flateart.htm">Dylan Gavel</a><br /></span>
+	
 </footer>
